377c828cdd71226e1933b6d8cc28c54d02979a5a
Franklin Chen <franklinchen@franklinchen.com>
2015-12-03 06:59:18 -0500

    Clearest way to write the HSpec code.

--- test/PCREHeavyExampleSpec.hs
+++ test/PCREHeavyExampleSpec.hs
@@ -2,7 +2,7 @@ module PCREHeavyExampleSpec where
 
 import PCREHeavyExample (mediaRegex)
 
-import Test.Hspec (Spec, hspec, describe, it, shouldSatisfy)
+import Test.Hspec (Spec, describe, it, shouldSatisfy)
 import Text.Regex.PCRE.Heavy ((=~))
 
 -- | Required for auto-discovery.
@@ -10,46 +10,16 @@ spec :: Spec
 spec =
   describe "pcre-heavy" $ do
     describe "match" $ do
-      mapM_ matchSpec matchExamples
+      it "has audio" $ do
+        "@Media:\thas-audio,   audio" `shouldSatisfy` (=~ mediaRegex)
+      it "has video" $ do
+        "@Media:\thas-video,video" `shouldSatisfy` (=~ mediaRegex)
+      it "has audio but missing" $ do
+        "@Media:\thas-audio-but-missing, audio, missing" `shouldSatisfy` (=~ mediaRegex)
+      it "has video but unlinked" $ do
+        "@Media:\thas-video-but-unlinked  , video,      unlinked" `shouldSatisfy` (=~ mediaRegex)
     describe "no match" $ do
-      mapM_ nonMatchSpec nonMatchExamples
-
-matchSpec :: (String, String) -> Spec
-matchSpec (description, text) =
-  it description $ do
-    text `shouldSatisfy` (=~ mediaRegex)
-
-nonMatchSpec :: (String, String) -> Spec
-nonMatchSpec (description, text) =
-  it description $ do
-    text `shouldSatisfy` (not . (=~ mediaRegex))
-
-matchExamples :: [(String, String)]
-matchExamples =
-  [ ( "has audio"
-    , "@Media:\thas-audio,   audio"
-    )
-  , ( "has video"
-    , "@Media:\thas-video,video"
-    )
-  , ( "has audio but missing"
-    , "@Media:\thas-audio-but-missing, audio, missing"
-    )
-  , ( "has video but unlinked"
-    , "@Media:\thas-video-but-unlinked  , video,      unlinked"
-    )
-  ]
-
-nonMatchExamples :: [(String, String)]
-nonMatchExamples =
-  [ ( "no audio or video"
-    , "@Media:\tno-audio-or-video"
-    )
-  , ( "missing media field"
-    , "@Media:\tmissing-media-field, unlinked"
-    )
-  ]
-
--- | Just for our convenience to manually run just this module's tests.
-main :: IO ()
-main = hspec spec
+      it "no audio or video" $ do
+        "@Media:\tno-audio-or-video" `shouldSatisfy` (not . (=~ mediaRegex))
+      it "missing media field" $ do
+        "@Media:\tmissing-media-field, unlinked" `shouldSatisfy` (not . (=~ mediaRegex))
