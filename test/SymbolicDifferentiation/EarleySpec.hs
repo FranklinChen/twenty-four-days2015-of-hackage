f1d4622d1351d839b85a616349ba655d73dce3df
Franklin Chen <franklinchen@franklinchen.com>
2015-12-17 13:56:13 -0500

    Fix Earley test.

--- test/SymbolicDifferentiation/EarleySpec.hs
+++ test/SymbolicDifferentiation/EarleySpec.hs
@@ -20,21 +20,17 @@ spec =
   describe "Custom syntax for expression parsed by Earley" $ do
     -- For simplicity, don't support negative numeric literals now.
     prop "x + a" $ \(NonNegative (a :: Int)) ->
-      Earley.parses [i|x + ${a}|] `shouldSatisfy`
-        \case
-          ([Plus (V "x") (N a)], _) -> True
-          _ -> False
+      fst (Earley.parses [i|x + ${a}|]) `shouldBe`
+        [Plus (V "x") (N a)]
 
     prop "x*a + y*b * (z+c)" $
       \(NonNegative (a :: Int))
        (NonNegative (b :: Int))
        (NonNegative (c :: Int)) ->
-      Earley.parses [i|x*${a} + y*${b} * (z+${c})|] `shouldSatisfy`
-        \case
-          ([Plus (Times (V "x") (N a))
-                 (Times (Times (V "y") (N b))
-                        (Plus (V "z") (N c)))], _) -> True
-          _ -> False
+      fst (Earley.parses [i|x*${a} + y*${b} * (z+${c})|]) `shouldBe`
+        [Plus (Times (V "x") (N a))
+              (Times (Times (V "y") (N b))
+                     (Plus (V "z") (N c)))]
 
     it "x + y * + 5" $
       Earley.parses "x + y * + 5" `shouldSatisfy`
