31741f3e72243af4e9e0391d02c1860828702d8b
Franklin Chen <franklinchen@franklinchen.com>
2016-02-28 21:53:38 -0500

    Update to LTS 5.5

--- app/PCREHeavyExampleMain.hs
+++ app/PCREHeavyExampleMain.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-5.4 --install-ghc runghc --package pcre-heavy
+-- stack --resolver lts-5.5 --install-ghc runghc --package pcre-heavy
 
 {-# LANGUAGE QuasiQuotes #-}
 
